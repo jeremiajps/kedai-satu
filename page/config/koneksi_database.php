752ee92f30c18743cbb05e02c3eb5b94331aae8b
jeremiajps <jeremiajosevanadiputrasulistyo@gmail.com>
2024-08-29 12:07:57 +0700

    Update koneksi database

--- page/config/koneksi_database.php
+++ page/config/koneksi_database.php
@@ -1,4 +1,4 @@
 <?php
-    $dbconn = pg_connect("host=localhost port=5432 dbname=rm_satu user=postgres password=195410002")
+    $dbconn = pg_connect("host=ce0lkuo944ch99.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com port=5432 dbname=ddgn5lcifpdfo4 user=ubgenp1uhebkcq password=p912c340494b27fa778e6577ac3756bdfd766350e563a36a8a3472c76481fdd47")
     or die('Could not connect: ' . pg_last_error());
 ?>
